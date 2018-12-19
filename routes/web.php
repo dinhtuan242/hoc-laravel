fefd177cb4fd418785314dc210a524868a22ff89
justinvu242 <vudinhtuan242@gmail.com>
2018-12-19 22:37:29 +0700

    Route nang cao

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,23 @@ Route::get('/contact', function () {
 Route::get('/help', function () {
     return 'Đây là trang trợ giúp';
 });
+Route::get('/about/facebook', function (){
+    return 'Trang about của facebook';
+});
+Route::any('facebook', function(){
+    return 'Route any test';
+});
+Route::get('/baidang/{id}', function($id){
+    return 'Bài đăng có id là: '.$id;
+});
+Route::get('/baidang/{price}/{art}', function($price, $art){
+   return 'Price: '.$price.' and art: '.$art;
+});
+// Dat ten cho route
+Route::get('about/test', array('as' => 'tests', function(){
+    $theURL = URL::route('tests');
+    return "Link: $theURL";
+}));
+Route::get('admin', function(){
+   return Redirect::route('tests');
+});
